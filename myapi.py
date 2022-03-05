e93b2821b1bae87fe98c941cfbcae34c692e4854
AndreLCSilva <andrelcsilva92@gmail.com>
2022-03-04 22:03:22 -0300

    API completa com os metódos GET, POST, PUT e DELETE para pesquisar em um banco de dados de alunos

--- myapi.py
+++ myapi.py
@@ -1,1 +1,70 @@
-import fastapi
+from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+students = {
+    1: {
+        "name": "John",
+        "age": "17",
+        "year": "year 12"
+    }
+}
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+class updateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+
+@app.get("/")
+def index():
+    return {"name": "first Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(None, description="The ID you want to view", gt=0)):
+    return students[student_id]
+
+@app.get("/get-by-name/{student_id}")
+def get_student(*, student_id: int, name: Optional[str] = None, test: int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Data already exists"}
+
+    students[student_id] = student
+    return students[student_id]
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: updateStudent):
+    if student_id not in students:
+        return {"Error": "Data not found"}
+    
+    if student.name != None:
+        students[student_id].name = student.name
+    
+    if student.age != None:
+        students[student_id].age = student.age
+    
+    if student.year != None:
+        students[student_id].year = student.year
+
+    return students[student_id]
+
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "There is no data"}
+    
+    del students[student_id]
+    return {"Message": "Data deleted"}
